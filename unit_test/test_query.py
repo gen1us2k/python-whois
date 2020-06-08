10f6b6711351709ddd1cc9b4750c1fa27c2da81d
Joey McAdams <jmcadams@rapid7.com>
2020-06-08 15:33:43 -0500

    running unit tests

--- unit_test/test_query.py
+++ unit_test/test_query.py
@@ -3,14 +3,14 @@ import whois
 
 class TestQuery(TestCase):
     def test_query(self):
-        # result = whois.query("stuff.com.br", ignore_returncode=1)
+        result = whois.query("stuff.com.br", ignore_returncode=1)
         result1 = whois.query("google.com.br", ignore_returncode=1)
-        # result2 = whois.query("www.google.com", ignore_returncode=1)
-        # result3 = whois.query("example.com.org", ignore_returncode=1)
-        # result4 = whois.query("rapid7.com", ignore_returncode=1)
+        result2 = whois.query("www.google.com", ignore_returncode=1)
+        result3 = whois.query("example.com.org", ignore_returncode=1)
+        result4 = whois.query("rapid7.com", ignore_returncode=1)
 
-        # self.assertIsNotNone(result)
+        self.assertIsNotNone(result)
         self.assertIsNotNone(result1)
-        # self.assertIsNotNone(result2)
-        # self.assertIsNotNone(result3)
-        # self.assertIsNotNone(result4)
+        self.assertIsNotNone(result2)
+        self.assertIsNotNone(result3)
+        self.assertIsNotNone(result4)
